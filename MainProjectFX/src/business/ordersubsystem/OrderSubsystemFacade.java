7f8c792db33c530f6938a99c4dad6429f5bbf96a
unknown <984176@NCSLab_219-302.cs.mum.edu>
2015-04-17 14:07:57 -0500

    Created method stubs for Order Subsystem

--- MainProjectFX/src/business/ordersubsystem/OrderSubsystemFacade.java
+++ MainProjectFX/src/business/ordersubsystem/OrderSubsystemFacade.java
@@ -1,13 +1,17 @@
 package business.ordersubsystem;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 import middleware.exceptions.DatabaseException;
+import business.exceptions.BackendException;
+import business.externalinterfaces.CartItem;
 import business.externalinterfaces.CustomerProfile;
 import business.externalinterfaces.Order;
 import business.externalinterfaces.OrderItem;
 import business.externalinterfaces.OrderSubsystem;
+import business.externalinterfaces.ShoppingCart;
 
 public class OrderSubsystemFacade implements OrderSubsystem {
 	private static final Logger LOG = 
@@ -46,4 +50,32 @@ public class OrderSubsystemFacade implements OrderSubsystem {
     	DbClassOrder dbClass = new DbClassOrder();
     	return dbClass.getOrderData(orderId);
     }
+
+
+    /** 
+     *  Used by customer subsystem at login to obtain this customer's order history from the database.
+	 *  Assumes cust id has already been stored into the order subsystem facade */
+	@Override
+	public List<Order> getOrderHistory() throws BackendException {
+		try{
+			List<Order> orders =  new ArrayList();
+			for(Integer orderId: getAllOrderIds()){
+				orders.add(getOrderData(orderId));
+			}
+			return orders;
+		}catch(DatabaseException e){
+			throw new BackendException(e);
+		}
+	}
+
+
+	/** 
+	 * Used by customer subsystem when a final order is submitted; this method extracts order and order items
+	 * from the passed in shopping cart and saves to database using data access subsystem
+	 */ 
+	@Override
+	public void submitOrder(ShoppingCart shopCart) throws BackendException {
+		// TODO Auto-generated method stub
+		
+	}
 }
