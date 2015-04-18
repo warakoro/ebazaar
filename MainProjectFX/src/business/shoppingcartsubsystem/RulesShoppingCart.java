efd34b080ce25c70912df58f2fa8d7b67a8999ae
Sanjay Tourani <tourani@utexas.edu>
2015-04-18 00:45:55 -0500

    Created RulesShoppingCart

---
+++ MainProjectFX/src/business/shoppingcartsubsystem/RulesShoppingCart.java
@@ -0,0 +1,72 @@
+package business.shoppingcartsubsystem;
+
+import java.util.HashMap;
+import java.util.List;
+
+import business.exceptions.BusinessException;
+import business.exceptions.RuleException;
+import business.externalinterfaces.DynamicBean;
+import business.externalinterfaces.Rules;
+import business.externalinterfaces.RulesConfigKey;
+import business.externalinterfaces.RulesConfigProperties;
+import business.externalinterfaces.RulesSubsystem;
+import business.externalinterfaces.ShoppingCart;
+import business.rulesbeans.ShopCartBean;
+import business.rulesubsystem.RulesSubsystemFacade;
+
+public class RulesShoppingCart implements Rules{
+	
+	private HashMap<String,DynamicBean> table;
+	private DynamicBean bean;	
+	private RulesConfigProperties config = new RulesConfigProperties();
+
+	public RulesShoppingCart(ShoppingCart sc) {
+		// TODO Auto-generated constructor stub
+		bean = new ShopCartBean(sc);
+	}
+	@Override
+	public String getModuleName() {
+		// TODO Auto-generated method stub
+		return config.getProperty(RulesConfigKey.SHOPCART_MODULE.getVal());
+	}
+
+	@Override
+	public String getRulesFile() {
+		// TODO Auto-generated method stub
+		return config.getProperty(RulesConfigKey.SHOPCART_RULES_FILE.getVal());
+	}
+
+	@Override
+	public void prepareData() {
+		// TODO Auto-generated method stub
+		table = new HashMap<String,DynamicBean>();		
+		String deftemplate = config.getProperty(RulesConfigKey.SHOPCART_DEFTEMPLATE.getVal());
+		table.put(deftemplate, bean);	
+	}
+
+	@Override
+	public HashMap<String, DynamicBean> getTable() {
+		// TODO Auto-generated method stub
+		return table;
+	}
+
+	@Override
+	public void runRules() throws BusinessException, RuleException {
+		// TODO Auto-generated method stub
+		RulesSubsystem rules = new RulesSubsystemFacade();
+    	rules.runRules(this);
+	}
+
+	@Override
+	public void populateEntities(List<String> updates) {
+		// do nothing
+		
+	}
+
+	@Override
+	public List<?> getUpdates() {
+		// do nothing
+		return null;
+	}
+
+}
