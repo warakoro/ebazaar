b00de56f33d83c4cc2987736dbc22b01ec9b1c23
mamadou <diarraoa@gmail.com>
2015-04-21 15:35:14 -0500

    Merged DONE 2

--- MainProjectFX/src/presentation/control/ViewOrdersUIControl.java
+++ MainProjectFX/src/presentation/control/ViewOrdersUIControl.java
@@ -1,9 +1,11 @@
 package presentation.control;
 
+import business.exceptions.UnauthorizedException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import presentation.data.*;
 import presentation.gui.OrderDetailWindow;
@@ -11,54 +13,88 @@ import presentation.gui.OrdersWindow;
 
 public enum ViewOrdersUIControl {
 	INSTANCE;
-	
+
 	private OrdersWindow ordersWindow;
 	private OrderDetailWindow orderDetailWindow;
-    private Stage primaryStage;
-    private Callback startScreenCallback;
-	
+	private Stage primaryStage;
+	private Callback startScreenCallback;
+
 	public void setPrimaryStage(Stage ps, Callback returnMessage) {
 		primaryStage = ps;
 		startScreenCallback = returnMessage;
 	}
-	
-	private class ViewOrdersHandler implements EventHandler<ActionEvent> {
-		@Override
-		public void handle(ActionEvent evt) {
-			ordersWindow = new OrdersWindow(primaryStage);
-			ordersWindow.setData(FXCollections.observableList(ViewOrdersData.INSTANCE.getOrders()));
-			ordersWindow.show();
-	        primaryStage.hide();			
-		}	
-	}
+
+	/*
+	 * private class ViewOrdersHandler implements EventHandler<ActionEvent> {
+	 * 
+	 * @Override public void handle(ActionEvent evt) { ordersWindow = new
+	 * OrdersWindow(primaryStage);
+	 * ordersWindow.setData(FXCollections.observableList
+	 * (ViewOrdersData.INSTANCE.getOrders())); ordersWindow.show();
+	 * primaryStage.hide(); } }
+	 */
 	public ViewOrdersHandler getViewOrdersHandler() {
 		return new ViewOrdersHandler();
 	}
-	
-	//OrdersWindow
+
+	// ////////\\\\\\\\\\\\\\\\\\\
+	private class ViewOrdersHandler implements EventHandler<ActionEvent>, Callback {
+		public void doUpdate() {
+			try {
+				Authorization.checkAuthorization(ordersWindow, DataUtil.custIsAdmin());
+			} catch (UnauthorizedException e) {
+				displayError(e.getMessage());
+				return;
+			}
+			ordersWindow.setData(FXCollections.observableList(ViewOrdersData.INSTANCE.getOrders()));
+			primaryStage.hide();
+			ordersWindow.show();
+		}
+
+		public Text getMessageBar() {
+			return startScreenCallback.getMessageBar();
+		}
+
+		@Override
+		public void handle(ActionEvent evt) {
+			ordersWindow = new OrdersWindow(primaryStage);
+			boolean isLoggedIn = DataUtil.isLoggedIn();
+			if (!isLoggedIn) {
+				LoginUIControl loginControl = new LoginUIControl(ordersWindow,
+						primaryStage, this);
+				loginControl.startLogin();
+			} else {
+				doUpdate();
+			}
+		}
+
+	}
+
+	// OrdersWindow
 	private class ViewOrderDetailsHandler implements EventHandler<ActionEvent> {
 		@Override
 		public void handle(ActionEvent evt) {
 			TableView<OrderPres> table = ordersWindow.getTable();
-			OrderPres selectedOrder = table.getSelectionModel().getSelectedItem();
-			if(selectedOrder == null) {
+			OrderPres selectedOrder = table.getSelectionModel()
+					.getSelectedItem();
+			if (selectedOrder == null) {
 				ordersWindow.displayError("Please select a row.");
 			} else {
 				ordersWindow.clearMessages();
 				orderDetailWindow = new OrderDetailWindow();
-				orderDetailWindow.setData(FXCollections.observableList(selectedOrder.getOrderItemsPres()));
+				orderDetailWindow.setData(FXCollections
+						.observableList(selectedOrder.getOrderItemsPres()));
 				ordersWindow.hide();
 				orderDetailWindow.show();
 			}
 		}
 	}
-	
+
 	public ViewOrderDetailsHandler getViewOrderDetailsHandler() {
 		return new ViewOrderDetailsHandler();
 	}
-	
-	
-	//OrderDetailWindow
+
+	// OrderDetailWindow
 	private class OrderDetailsOkHandler implements EventHandler<ActionEvent> {
 		@Override
 		public void handle(ActionEvent evt) {
@@ -66,18 +102,20 @@ public enum ViewOrdersUIControl {
 			orderDetailWindow.hide();
 		}
 	}
+
 	public OrderDetailsOkHandler getOrderDetailsOkHandler() {
 		return new OrderDetailsOkHandler();
 	}
-	
+
 	private class CancelHandler implements EventHandler<ActionEvent> {
 		public void handle(ActionEvent evt) {
 			ordersWindow.hide();
 			startScreenCallback.clearMessages();
 			primaryStage.show();
-			
+
 		}
 	}
+
 	public CancelHandler getCancelHandler() {
 		return new CancelHandler();
 	}
